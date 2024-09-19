189019df68861d8a7b5425dc02ac346dd045138e
tracer-ics2024 <tracer@njuics.org>
2024-09-19 15:56:51 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:56:51 up  2:01,  1 user,  load average: 0.74, 1.05, 1.23

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -81,7 +81,7 @@ static int cmd_info(char *args)
 {
   //
   //printf("成功进入info函数\n");
-  if (args==NULL || (strcmp(args,"r")!=0 && strcmp(args,"w")!=0)){assert(0);return -1;}//考虑到用户可能故意刁难在info后面不写参数或者写了一大堆乱七八糟的参数，这里需要判断一下
+  if (args==NULL || (strcmp(args,"r")!=0 && strcmp(args,"w")!=0)){return -1;}//考虑到用户可能故意刁难在info后面不写参数或者写了一大堆乱七八糟的参数，这里需要判断一下
   if (strcmp(args,"r")==0)
   {
     //如果参数输入的是r的话打印寄存器信息
@@ -126,9 +126,9 @@ return 0;
 //表达式求值
 static int cmd_p(char *args) {
   bool success=true;
-	int res = expr(args,&success);
+	word_t res = expr(args,&success);
   if(!success){return -1;}
-  printf("表达式求值结果为：%d\n",res);
+  printf("表达式求值结果为：%u\n",res);
   return 0;
 }
 
