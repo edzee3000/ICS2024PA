b47ec7b39377feb5e914a7a2e55bb3928dde96a9
tracer-ics2024 <tracer@njuics.org>
2024-09-19 18:44:54 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:44:54 up  1:12,  1 user,  load average: 1.03, 0.91, 0.80

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -177,6 +177,9 @@ static bool make_token(char *e) {
               break;
               } 
           }
+
+
+
           // if(idx==num_regs)assert(0);//表示输入寄存器名字有问题
           if(idx==num_regs){printf("输入的寄存器名字有问题\n");return false;}
           break;
@@ -210,14 +213,21 @@ word_t expr(char *e, bool *success) {
   judge_DEREF(i);
   judge_NEG(i);
   }
-  // for(i=0;i<nr_token;i++)//打印类型内容
-  // {
-  //   if(tokens[i].type==DECIMAL_NUM){printf("toke%d类型为:%d ,内容为：%d\n",i,tokens[i].type, atoi(tokens[i].str));}
-  //   else if(tokens[i].type==HEX_NUM||tokens[i].type==REGISTER){ u_int32_t value;
-  //     sscanf(tokens[i].str,"%x",&value);
-  //     printf("toke%d类型为:%d ,内容为：%#x\n",i,tokens[i].type,value);}
-  //   else{printf("toke%d类型为:%d\n",i,(char)tokens[i].type);}
-  // }
+
+
+
+  for(i=0;i<nr_token;i++)//打印类型内容//之后是需要注释掉的的
+  {
+    if(tokens[i].type==DECIMAL_NUM){printf("toke%d类型为:%d ,内容为：%d\n",i,tokens[i].type, atoi(tokens[i].str));}
+    else if(tokens[i].type==HEX_NUM||tokens[i].type==REGISTER){ u_int32_t value;
+      sscanf(tokens[i].str,"%x",&value);
+      printf("toke%d类型为:%d ,内容为：%#x\n",i,tokens[i].type,value);}
+    else{printf("toke%d类型为:%d\n",i,(char)tokens[i].type);}
+  }
+
+
+
+
   //准备工作做完之后开始计算表达式的值
   u_int32_t res= eval(0,nr_token-1);
 
@@ -318,6 +328,7 @@ int eval(int p,int q) {
     case DECIMAL_NUM:
       u_int32_t value;
       sscanf(tokens[p].str,"%d",&value);
+      printf("十进制数为：%u",value);
       return value;break;
     case HEX_NUM: 
       sscanf(tokens[p].str,"%x",&value);
