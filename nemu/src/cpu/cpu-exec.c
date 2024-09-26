ed1427db66b9176e33c2ab68838c8b706352cedf
231300027-Zhu Shihang <231300027@smail.nju.edu.cn>
2024-09-26 16:31:22 +0800

    今天RTFSC了一部分，然后编译dummy程序，报了第二个错误以及python not found的错误，已经成功解决   然后突然看到实验课网站上还有lab的实验  遂中止PA  先去把Lab1解决掉

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -63,7 +63,9 @@ static void exec_once(Decode *s, vaddr_t pc) {
   //但可以说明的是, 它会随着取指的过程修改s->snpc的值, 使得从isa_exec_once()返回后s->snpc正好为下一条指令的PC.
   isa_exec_once(s);
   //接下来代码将会通过s->dnpc来更新PC, 这里的dnpc是"dynamic next PC"的意思. 关于snpc和dnpc的区别, 我们会在下文进行说明.
-  cpu.pc = s->dnpc;
+  cpu.pc = s->dnpc;//最后是更新PC. 更新PC的操作非常简单, 只需要把s->dnpc赋值给cpu.pc即可. 
+  //现在来说明一下snpc和dnpc的区别：在程序分析领域中, 静态指令是指程序代码中的指令, 动态指令是指程序运行过程中的指令  jmp指令的下一条静态指令是add指令, 而下一条动态指令则是xor指令.
+  //对于顺序执行的指令, 它们的snpc和dnpc是一样的; 但对于跳转指令, snpc和dnpc就会有所不同, dnpc应该指向跳转目标的指令. 显然, 我们应该使用s->dnpc来更新PC, 并且在指令执行的过程中正确地维护s->dnpc.
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
