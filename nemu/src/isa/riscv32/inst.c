9f0d6e1c4e145823c9211b54a259cc5b3d983e59
tracer-ics2024 <tracer@njuics.org>
2024-10-07 21:40:09 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:40:09 up  9:10,  1 user,  load average: 0.54, 0.67, 0.77

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -128,7 +128,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 101 ????? 01100 11", divu  ,R,{R(rd)=(uint32_t)src1/(uint32_t)src2;});
   INSTPAT("0000001 ????? ????? 110 ????? 01100 11", rem   ,R,{R(rd)=(int)src1%(int)src2;});
   INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu  ,R,{R(rd)=(uint32_t)src1%(uint32_t)src2;});
-  INSTPAT("0000001 ????? ????? 001 ????? 01100 11", mulh  ,R,{printf("src1:%#llx\n",(long long int)src1);R(rd)=(int32_t)(((int64_t)src1*(int64_t)src2)>>32);});//############可能会出问题##############
+  INSTPAT("0000001 ????? ????? 001 ????? 01100 11", mulh  ,R,{src1=((long long int)src1)<<32;printf("src1:%#llx\n",(long long int)(src1));R(rd)=(int32_t)(((int64_t)src1*(int64_t)src2)>>32);});//############可能会出问题##############
 
 
 
