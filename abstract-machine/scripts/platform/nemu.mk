d749325672a6e43d3a7b3bc7ee32a8e52aa96bd0
tracer-ics2024 <tracer@njuics.org>
2024-10-07 19:21:50 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:21:50 up  6:52,  1 user,  load average: 0.80, 1.01, 0.97

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -13,7 +13,7 @@ LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt 
-# NEMUFLAGS += -b #注意这里添加了-b作为批处理参数
+NEMUFLAGS += -b #注意这里添加了-b作为批处理参数
 # NEMUFLAGS += -e $(IMAGE).elf
 
 MAINARGS_MAX_LEN = 64
