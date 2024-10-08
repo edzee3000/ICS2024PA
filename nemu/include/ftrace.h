d1211cd6269ddfe60de15a2bf9ba245decad5110
tracer-ics2024 <tracer@njuics.org>
2024-10-08 13:06:20 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:06:20 up 35 min,  1 user,  load average: 1.89, 1.53, 1.17

---
+++ nemu/include/ftrace.h
@@ -0,0 +1,20 @@
+#ifndef __FTRACE_H__
+#define __FTRACE_H__
+
+// Located at src/isa/$(GUEST_ISA)/include/isa-def.h
+
+#include <common.h>
+#include <elf.h>
+#include <isa.h>
+#include <memory/paddr.h>
+
+typedef struct {
+	char name[32]; // 函数名,32字节应该足够了
+	vaddr_t addr;
+	unsigned char info;
+	Elf32_Xword size;
+} SymEntry;
+
+
+
+#endif
---
+++ nemu/src/monitor/ftrace.c
@@ -0,0 +1,10 @@
+#include <common.h>
+#include <isa.h>
+#include <elf.h>
+#include <ftrace.h>
+#include <memory/paddr.h>
+
+
+SymEntry *symbol_tbl = NULL; // dynamic allocated
+
+
