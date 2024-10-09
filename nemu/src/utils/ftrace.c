5b655882526ac46a1238932ae81cd24afcec7ff6
tracer-ics2024 <tracer@njuics.org>
2024-10-09 17:39:19 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:39:19 up  1:32,  1 user,  load average: 2.83, 1.89, 1.73

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -68,9 +68,7 @@ void parse_elf(const char *elf_file) {
       size_t num_symbols = shdr[i].sh_size / shdr[i].sh_entsize;// 计算符号表的条目数量
       //fseek(file, shdr[i].sh_offset, SEEK_SET);// 读取符号表条目
       // 读取字符串表
-      //fseek(file, shdr[i].sh_offset, SEEK_SET);
-      fseek(file, shdr[i].sh_link * sizeof(Elf32_Shdr) + ehdr.e_shoff, SEEK_SET);
-      
+      fseek(file, shdr[i].sh_offset, SEEK_SET);
       char* string_table = (char *)malloc(shdr[i].sh_size);
       if (fread(string_table, shdr[i].sh_size, 1, file) != 1) {
           perror("读取字符串表发生错误");
@@ -79,6 +77,7 @@ void parse_elf(const char *elf_file) {
           exit(EXIT_FAILURE);
       }
       //printf("偏移量为:%d\n",shdr[i].sh_offset);
+      printf("节头大小为:%d\n",shdr[i].sh_size);
       printf("string_table为:%s\n",string_table);
       fseek(file, shdr[i].sh_offset, SEEK_SET);
       for (size_t j = 0; j < num_symbols; j++) {//循环遍历符号表寻找STT_FUNC
