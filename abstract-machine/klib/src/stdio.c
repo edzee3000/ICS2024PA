0b80ae031a4fa854f587543aa870d41e098fe956
tracer-ics2024 <tracer@njuics.org>
2024-10-11 22:14:11 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:14:11 up  4:58,  1 user,  load average: 0.93, 0.83, 0.79

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,7 +2,7 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
-#include <stdlib.h>
+// #include <stdlib.h>
 // #include <regex.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
@@ -19,6 +19,8 @@
 //    */
 //   {}
 // };
+char* itoa(int value,char*string,int radix);//value: 要转换的整数，string: 转换后的字符串,radix: 转换进制数，如2,8,10,16 进制等。
+
 
 int printf(const char *fmt, ...) {
   //panic("Not implemented");
@@ -60,9 +62,10 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
       if(p[i]=='d'||p[i]=='i')
       {
         int num=va_arg(ap,int);
-        char tem[128];int k=0;
-        while(num!=0){int t=num%10;num/=10;tem[k]='0'+t; k++;} tem[k]='\0';
-        for(int l=k-1;l>=0;l--){out[j]=tem[l]; j++;       if(j>=n)break; } 
+        char str[16];
+        itoa(num,str,10);
+        char *ch=str;
+        while(*ch!='\0'){out[j]=*ch;ch++;j++;     if(j>=n)break;}
       }
       else if( p[i]=='c'){//输入一个字符
         char c=va_arg(ap,int);
@@ -74,9 +77,11 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
         const char *ch=va_arg(ap,const char*);
         while(*ch!='\0'){out[j]=*ch;ch++;j++;     if(j>=n)break;}
       }
-      else if( p[i]<='9'&&p[i]>='0'  && p[i+1]=='2'  &&p[i+2]=='d' ){i+=2;  int num=va_arg(ap,int);char tem[10];int k=0;
-        while(num!=0){int t=num%10;num/=10;tem[k]='0'+t; k++;} tem[k]='\0';
-        for(int l=k-1;l>=0;l--){out[j]=tem[l]; j++;       if(j>=n)break; }  }
+      else if( p[i]<='9'&&p[i]>='0'  && p[i+1]=='2'  &&p[i+2]=='d' ){i+=2;  int num=va_arg(ap,int);
+        char str[16];
+        itoa(num,str,10);
+        char *ch=str;
+        while(*ch!='\0'){out[j]=*ch;ch++;j++;     if(j>=n)break;} }  
 
 
 
@@ -98,5 +103,45 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
   return vsnprintf(out,n,fmt,args);
 }
 
+
+
+char* itoa(int num,char* str,int radix)
+{
+	char index[]="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";//索引表
+	unsigned unum;//存放要转换的整数的绝对值,转换的整数可能是负数
+	int i=0,j,k;//i用来指示设置字符串相应位，转换之后i其实就是字符串的长度；转换后顺序是逆序的，有正负的情况，k用来指示调整顺序的开始位置;j用来指示调整顺序时的交换。
+ 
+	//获取要转换的整数的绝对值
+	if(radix==10&&num<0)//要转换成十进制数并且是负数
+	{
+		unum=(unsigned)-num;//将num的绝对值赋给unum
+		str[i++]='-';//在字符串最前面设置为'-'号，并且索引加1
+	}
+	else unum=(unsigned)num;//若是num为正，直接赋值给unum
+ 
+	//转换部分，注意转换后是逆序的
+	do
+	{
+		str[i++]=index[unum%(unsigned)radix];//取unum的最后一位，并设置为str对应位，指示索引加1
+		unum/=radix;//unum去掉最后一位
+ 
+	}while(unum);//直至unum为0退出循环
+ 
+	str[i]='\0';//在字符串最后添加'\0'字符，c语言字符串以'\0'结束。
+ 
+	//将顺序调整过来
+	if(str[0]=='-') k=1;//如果是负数，符号不用调整，从符号后面开始调整
+	else k=0;//不是负数，全部都要调整
+ 
+	char temp;//临时变量，交换两个值时用到
+	for(j=k;j<=(i-1)/2;j++)//头尾一一对称交换，i其实就是字符串的长度，索引最大值比长度少1
+	{
+		temp=str[j];//头部赋值给临时变量
+		str[j]=str[i-1+k-j];//尾部赋值给头部
+		str[i-1+k-j]=temp;//将临时变量的值(其实就是之前的头部值)赋给尾部
+	}
+ 
+	return str;//返回转换后的字符串
+}
 
 #endif
