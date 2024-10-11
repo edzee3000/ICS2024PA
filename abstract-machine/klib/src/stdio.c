e36201eda80a5130e661652b78293cbf88896cbc
tracer-ics2024 <tracer@njuics.org>
2024-10-11 22:03:38 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:03:38 up  4:48,  1 user,  load average: 0.86, 0.79, 0.74

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -77,11 +77,11 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
         const char *ch=va_arg(ap,const char*);
         while(*ch!='\0'){out[j]=*ch;ch++;j++;     if(j>=n)break;}
       }
-      else if( p[i]<='9'&&p[i]>='0'  && p[i+1]=='2'  &&p[i+2]=='d' ){i+=2;  int num=va_arg(ap,int);
-        char str[16];
-        itoa(num,str,10);
-        char *ch=str;
-        while(*ch!='\0'){out[j]=*ch;ch++;j++;     if(j>=n)break;} }  
+      // else if( p[i]<='9'&&p[i]>='0'  && p[i+1]=='2'  &&p[i+2]=='d' ){i+=2;  int num=va_arg(ap,int);
+      //   char str[16];
+      //   itoa(num,str,10);
+      //   char *ch=str;
+      //   while(*ch!='\0'){out[j]=*ch;ch++;j++;     if(j>=n)break;} }  
 
 
 
