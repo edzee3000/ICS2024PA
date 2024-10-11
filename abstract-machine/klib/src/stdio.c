41704a2033b1a418a33b675db2c0b6028a4f274c
tracer-ics2024 <tracer@njuics.org>
2024-10-11 19:37:53 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:37:53 up  2:22,  1 user,  load average: 1.09, 0.91, 0.85

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,8 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 // #if !defined(__ISA_NATIVE__) || !defined(__NATIVE_USE_KLIB__)
+#define MAX_LEN_INPUT (uint32_t)(65536)
+
 int printf(const char *fmt, ...) {
   //panic("Not implemented");
   va_list args;
@@ -18,35 +20,8 @@ int printf(const char *fmt, ...) {
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   //panic("Not implemented");
-  const char *p=fmt;
-  size_t i=0;
-  size_t j=0;
-  while(p[i]!='\0')
-  {
-    if(p[i]=='%')
-    {
-      i++;
-      switch (p[i])
-      {
-      case 'd': case 'i': case 'c'://输入一个整数
-        int num=va_arg(ap,int);
-        char tem[128];int k=0;
-        while(num!=0){int t=num%10;num/=10;tem[k]='0'+t; k++;} tem[k]='\0';
-        for(int l=k-1;l>=0;l--){out[j]=tem[l]; j++;} 
-        break;
-      case 's'://输入一个字符串
-        const char *ch=va_arg(ap,const char*);
-        while(*ch!='\0'){out[j]=*ch;ch++;j++;}
-        break;
-      default:break;
-      }
-    }
-    else{out[j]=p[i];j++;}
-    i++;
-  }
-  out[j]='\0';
-  //在 stdio.h 中，sprintf 与 vsprintf 函数的返回值是成功写入缓冲区的字符数量。这个返回值不包括终止的空字符（\0）。如果发生输出错误，函数会返回一个负数。
-  return j;
+  uint32_t n=MAX_LEN_INPUT;
+  return vsnprintf(out,n,fmt,ap);
 }
 
 
@@ -63,12 +38,42 @@ int sprintf(char *out, const char *fmt, ...) {
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   //panic("Not implemented");
+  
   return 0;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   //panic("Not implemented");
-  return 0;
+  const char *p=fmt;
+  size_t i=0;
+  size_t j=0;
+  while(p[i]!='\0' && j<n)
+  {
+    if(p[i]=='%')
+    {
+      i++;
+      switch (p[i])
+      {
+      case 'd': case 'i': case 'c'://输入一个整数
+        int num=va_arg(ap,int);
+        char tem[128];int k=0;
+        while(num!=0){int t=num%10;num/=10;tem[k]='0'+t; k++;} tem[k]='\0';
+        for(int l=k-1;l>=0;l--){out[j]=tem[l]; j++;       if(j>=n)break; } 
+        break;
+      case 's'://输入一个字符串
+        const char *ch=va_arg(ap,const char*);
+        while(*ch!='\0'){out[j]=*ch;ch++;j++;            if(j>=n)break;}
+        break;
+      default:break;
+      }
+    }
+    else{out[j]=p[i];j++;}
+    i++;
+    if(j>=n)break;
+  }
+  out[j]='\0';
+  //在 stdio.h 中，sprintf 与 vsprintf 函数的返回值是成功写入缓冲区的字符数量。这个返回值不包括终止的空字符（\0）。如果发生输出错误，函数会返回一个负数。
+  return j;
 }
 
 #endif
