462a7930a07896327b69bd9dae4ee54742b3bbbb
tracer-ics2024 <tracer@njuics.org>
2024-10-18 17:47:35 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:47:35 up  5:31,  1 user,  load average: 0.44, 0.66, 0.82

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -60,7 +60,7 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
   }
   //如果一切检查都没有问题的话就创建新的map并且赋值给新id的map
   maps[nr_map] = (IOMap){ .name = name, .low = addr, .high = addr + len - 1,
-    .space = space, .callback = callback };
+    .space = space, .callback = callback };//新的map占用内存范围为 [low,high]  之间 
   Log("Add mmio map '%s' at [" FMT_PADDR ", " FMT_PADDR "]",
       maps[nr_map].name, maps[nr_map].low, maps[nr_map].high);//这里有一个Log语句表示添加内存映射输入输出map映射
   //更新设备接口的个数
@@ -74,7 +74,7 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
 /* bus interface 总线接口*/
 word_t mmio_read(paddr_t addr, int len) {
   //########在这里添加dtrace语句####################################################################################
-  IOMap* map=fetch_mmio_map(addr);
+  IOMap* map=fetch_mmio_map(addr); //获得addr对应的map映射，  通过索引id去寻找，  比如如果addr在serial的索引范围之内就返回serial串口对应的map
   IFDEF(CONFIG_DTRACE,trace_dread(addr,len,map));
   return map_read(addr, len, map);
 }
