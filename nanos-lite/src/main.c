43e67cafd3e8723e8af5a0739ad4344ed827c943
tracer-ics2024 <tracer@njuics.org>
2024-10-29 22:14:12 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:14:12 up  1:43,  1 user,  load average: 0.68, 0.79, 0.60

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -7,10 +7,10 @@ void init_irq(void);
 void init_fs(void);
 void init_proc(void);
 
-int main() {
-  extern const char logo[];
+int main() {//简单梳理一下Nanos-lite目前的行为
+  extern const char logo[];//打印Project-N的logo, 并通过Log()输出hello信息和编译时间
   printf("%s", logo);
-  Log("'Hello World!' from Nanos-lite");
+  Log("'Hello World!' from Nanos-lite");   
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
   init_mm();
