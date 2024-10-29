48238fc9e1242f5c74dea64607e77d5dc935fe40
tracer-ics2024 <tracer@njuics.org>
2024-10-29 22:20:19 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:20:19 up  1:49,  1 user,  load average: 0.49, 0.72, 0.63

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -15,15 +15,15 @@ int main() {//简单梳理一下Nanos-lite目前的行为
 
   init_mm();
 
-  init_device();
+  init_device();//调用init_device()对设备进行一些初始化操作. 目前init_device()会直接调用ioe_init().
 
-  init_ramdisk();
+  init_ramdisk();//初始化ramdisk. 一般来说, 程序应该存放在永久存储的介质中(比如磁盘). 但要在NEMU中对磁盘进行模拟是一个略显复杂工作, 因此先让Nanos-lite把其中的一段内存作为磁盘来使用. 这样的磁盘有一个专门的名字, 叫ramdisk.
 
 #ifdef HAS_CTE
   init_irq();
 #endif
 
-  init_fs();
+  init_fs();//init_fs()和init_proc(), 分别用于初始化文件系统和创建进程, 目前它们均未进行有意义的操作, 可以忽略它们.
 
   init_proc();
 
@@ -33,5 +33,5 @@ int main() {//简单梳理一下Nanos-lite目前的行为
   yield();
 #endif
 
-  panic("Should not reach here");
+  panic("Should not reach here");//调用panic()结束Nanos-lite的运行.
 }
