b7a3431c47971a7a947e2858ffaa6fbd9823c32e
tracer-ics2024 <tracer@njuics.org>
2024-10-29 18:16:39 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:16:39 up  6:05,  1 user,  load average: 1.04, 1.03, 1.05

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -92,7 +92,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 }
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
-  if (!isa_difftest_checkregs(ref, pc)) {
+  if (!isa_difftest_checkregs(ref, pc)) {  //调用nemu/src/isa/riscv32/difftest/dut.c当中的isa_difftest_checkregs函数，看是否比较成功
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
     isa_reg_display();
