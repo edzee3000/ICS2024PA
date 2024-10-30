46bfbd612c389d206e016717e0535652f2703207
tracer-ics2024 <tracer@njuics.org>
2024-10-30 17:00:46 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:00:46 up 11 min,  1 user,  load average: 1.29, 1.13, 0.67

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,10 +2,12 @@
 
 static Context* do_event(Event e, Context* c) {
   //Nanos-lite的事件处理回调函数默认不处理所有事件, 你需要在其中识别出自陷事件EVENT_YIELD, 然后输出一句话即可, 目前无需进行其它操作
-  printf("自陷事件的编号为:%d\n",e.event);
+  //重新运行Nanos-lite, 如果你的实现正确, 你会看到识别到自陷事件之后输出的信息, 并且最后仍然触发了main()函数末尾设置的panic().
+  
+  // printf("自陷事件的编号为:%d\n",e.event);
 
   switch (e.event) {
-    
+    case EVENT_YIELD:printf("识别到自陷事件\n");    break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
