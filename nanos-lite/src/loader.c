03369ccfc87dd1d2fbf746c4461d7e52470748cf
tracer-ics2024 <tracer@njuics.org>
2024-10-30 19:12:44 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:12:44 up  2:23,  1 user,  load average: 0.48, 0.55, 0.63

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,6 +12,27 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+// 你很有可能会因为疏忽, 从而让native的Nanos-lite来加载运行一个x86/mips32/riscv32的dummy. 
+// 从ISA规范的角度来说, 这种行为显然属于UB, 具体而言通常会发生一些难以理解的错误. 
+// 为了避免这种情况, 可以在loader中检测ELF文件的ISA类型.
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_TYPE EM_X86_64
+// see /usr/include/elf.h to get the right type
+#elif defined(__ISA_MIPS32__) 
+# define EXPECT_TYPE EM_MIPS_X   
+//#define EM_MIPS_RS3_LE  10      /* MIPS R3000 little-endian */
+//#define EM_MIPS_X       51      /* Stanford MIPS-X */
+#elif defined(__ISA_LOONGARCH32R__)
+# define EXPECT_TYPE 
+//竟然找不到…… 
+#elif defined(__riscv)
+# define EXPECT_TYPE EM_RISCV
+#else
+# error Unsupported ISA
+#endif
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
   // 你需要在Nanos-lite中实现loader的功能, 来把用户程序加载到正确的内存位置, 然后执行用户程序.
@@ -24,7 +45,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr* elf=&elf_header;
   ramdisk_read(elf, 0, sizeof(Elf_Ehdr));//读取elf的header
   // assert(*(uint32_t *)elf->e_ident ==  0x7f454c46);//Linux/GNU 上的 ELF 文件的魔数是 0x7F 'E' 'L' 'F'，即十六进制的 7f 45 4c 46。  话说0x7f454c46是怎么摆放的？？？
-  assert(*(uint32_t *)elf->e_ident ==  0x464c457f);
+  assert(*(uint32_t *)elf->e_ident ==  0x464c457f); //好吧我承认原来是因为小端模式  e_ident里面有16个字节的内容  读数据的时候是从小到大摆放  强制类型转换为uint32_t *之后一次读取字节为4个字节但是解引用之后高位确实为0x46 低位确实为0x7f
 
   Elf_Phdr ProgramHeaders[elf->e_phnum];
   ramdisk_read(ProgramHeaders, elf->e_phoff, sizeof(Elf_Phdr)*elf->e_phnum);//读取程序头表（即段头表）一共要读取Elf_Phdr大小乘以程序头表个数
