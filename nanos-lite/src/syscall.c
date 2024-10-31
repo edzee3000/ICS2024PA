8da9b8bf9f07026938b8f8f775a360f533490683
tracer-ics2024 <tracer@njuics.org>
2024-10-31 11:06:24 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:06:24 up 42 min,  1 user,  load average: 0.98, 0.84, 0.82

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -69,7 +69,7 @@ void System_Trace(Context* c)
 {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  printf("STRACE得到系统调用信息:\t");
+  printf("STRACE:\t");
   switch (a[0]) {
     //你需要实现SYS_exit系统调用（case 0的情况）, 它会接收一个退出状态的参数. 为了方便测试, 我们目前先直接使用这个参数调用halt().    halt(0)表示成功退出 其余均为失败退出
     case SYS_exit: printf("系统调用编号:%d\t系统调用:SYS_exit\t返回值:c->GPRx=0\n",a[0]);  break;//对于c->mcause=1的情况，查看navy-apps/libs/libos/src/syscall.h对应为SYS_exit系统退出
