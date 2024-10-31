aa90c3e44e5473493d1465f6118f64b3e0882d41
tracer-ics2024 <tracer@njuics.org>
2024-10-31 11:05:17 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:05:17 up 41 min,  1 user,  load average: 0.59, 0.76, 0.79

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,8 +2,8 @@
 #include "syscall.h"
 
 
-#include <strace.h>
-void System_Trace(Context* c);
+
+
 
 /*本来想在Kconfig里面设置strace的开关的  结果好像没啥用处  因此在这里我手动添加这个CONFIG_STRACE参数
 config STRACE
@@ -15,8 +15,11 @@ config STRACE_COND
   string "Only trace System Call when the condition is true"
   default "true"
 */
-
 // #define CONFIG_STRACE
+void System_Trace(Context* c);
+
+
+
 
 void do_syscall(Context *c) {
   // Nanos-lite收到系统调用事件之后, 就会调出系统调用处理函数do_syscall()进行处理. 
@@ -36,4 +39,41 @@ void do_syscall(Context *c) {
   }
   
   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//strace代码放在这里好了  因为新开一个新的strace.c文件出问题了最后……
+void System_Trace(Context* c)
+{
+  uintptr_t a[4];
+  a[0] = c->GPR1;
+  printf("STRACE得到系统调用信息:\t");
+  switch (a[0]) {
+    //你需要实现SYS_exit系统调用（case 0的情况）, 它会接收一个退出状态的参数. 为了方便测试, 我们目前先直接使用这个参数调用halt().    halt(0)表示成功退出 其余均为失败退出
+    case SYS_exit: printf("系统调用编号:%d\t系统调用:SYS_exit\t返回值:c->GPRx=0\n",a[0]);  break;//对于c->mcause=1的情况，查看navy-apps/libs/libos/src/syscall.h对应为SYS_exit系统退出
+    case SYS_yield:printf("系统调用编号:%d\t系统调用:SYS_yield\t返回值:c->GPRx=%d\n",a[0], c->GPRx); break;  //c->mcause为系统调用SYS_yield的情况
+    default: panic("Unhandled syscall ID = %d", a[0]);
+  }
 }
