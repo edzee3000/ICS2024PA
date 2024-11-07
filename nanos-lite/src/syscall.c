5080461be911fe95a2563149243339285082f68f
tracer-ics2024 <tracer@njuics.org>
2024-11-07 21:51:25 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:51:25 up  4:32,  1 user,  load average: 0.87, 0.98, 1.04

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,7 +13,7 @@ config STRACE_COND
   string "Only trace System Call when the condition is true"
   default "true"
 */
-#define CONFIG_STRACE
+// #define CONFIG_STRACE
 void System_Trace(Context* c);
 
 size_t system_write(int fd, intptr_t buf, size_t count);
