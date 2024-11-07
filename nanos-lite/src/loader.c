a29b3ea015f1530fae99bed4d29cbab59b764b77
tracer-ics2024 <tracer@njuics.org>
2024-11-07 15:40:30 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:40:30 up  1:48,  1 user,  load average: 1.48, 1.24, 1.16

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -90,7 +90,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  printf("filename为:%s\n",filename);
+  // printf("filename为:%s\n",filename);
   // 实现后, 在init_proc()中调用naive_uload(NULL, NULL), 它会调用你实现的loader来加载第一个用户程序, 
   // 然后跳转到用户程序中执行. 如果你的实现正确, 你会看到执行dummy程序时在Nanos-lite中触发了一个未处理的4号事件. 
   // 这说明loader已经成功加载dummy, 并且成功地跳转到dummy中执行了
