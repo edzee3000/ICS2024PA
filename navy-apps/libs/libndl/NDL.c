dc75d8dc2def0c77c1b6424bd2c46e7a04da43a1
tracer-ics2024 <tracer@njuics.org>
2024-11-08 17:50:53 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:50:53 up  2:27,  1 user,  load average: 0.82, 0.52, 0.42

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -4,12 +4,23 @@
 #include <string.h>
 #include <unistd.h>
 
+#include "sys/time.h"
+#include <time.h>
+#include "assert.h"
+
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
+enum{SDL_INIT_TIMER,SDL_INIT_AUDIO,SDL_INIT_VIDEO,
+SDL_INIT_JOYSTICK,SDL_INIT_HAPTIC,SDL_INIT_GAMECONTROLLER,
+SDL_INIT_EVENTS,SDL_INIT_NOPARACHUTE,SDL_INIT_EVERYTHING};
+
 uint32_t NDL_GetTicks() {
-  return 0;
+  //SDL_GetTicks这个函数功能返回自从SDL库初始化到现在经过的毫秒值
+  struct timeval tv;
+  assert(gettimeofday(&tv, NULL) == 0);
+  return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
 
 int NDL_PollEvent(char *buf, int len) {
@@ -54,6 +65,22 @@ int NDL_QueryAudio() {
 }
 
 int NDL_Init(uint32_t flags) {
+  // 首先明确一下gettimeofday系统调用相关的ioe无论是使用clock_gettime还是gettimeofday（linux提供的版本，不是我们自己的），
+  // 返回的时间都不是从0开始，前者是宿主机器开机到现在经过的时间，后者是从1970到现在经过的时间。
+  // 所以我们还得保存一个NDL初始化时的时间，以便实现自从SDL库初始化到现在经过的时间的功能。
+
+  //int SDLCALL SDL_Init(Uint32 flags)
+  // 其中，flags可以取下列值：
+  // SDL_INIT_TIMER：定时器
+  // SDL_INIT_AUDIO：音频
+  // SDL_INIT_VIDEO：视频
+  // SDL_INIT_JOYSTICK：摇杆
+  // SDL_INIT_HAPTIC：触摸屏
+  // SDL_INIT_GAMECONTROLLER：游戏控制器
+  // SDL_INIT_EVENTS：事件
+  // SDL_INIT_NOPARACHUTE：不捕获关键信号（这个不理解）
+  // SDL_INIT_EVERYTHING：包含上述所有选项
+
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -2,19 +2,37 @@
 #include "assert.h"
 #include <sys/time.h>
 #include <time.h>
+// 以毫秒为单位返回系统时间
+#include "../../libs/libndl/include/NDL.h"
 
 int main() {
-  struct timeval tv;
+  // struct timeval tv;
   // struct timezone tz;
-  gettimeofday(&tv, NULL);
-  int ms = 500;
+  uint32_t ms = NDL_GetTicks()+500;
   while (1) {
-    while ((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ms) {
-      gettimeofday(&tv, NULL);
-    }
-    printf("ms = %d\n", ms);
+    uint32_t now_ms = NDL_GetTicks();
+    do{
+      now_ms = NDL_GetTicks();
+    }while(now_ms < ms); 
+    printf("ms = %u\n", ms);
     ms += 500;
   }
   printf("执行完timer-test\n");
   return 0;
 }
+
+// int main() {
+//   struct timeval tv;
+//   // struct timezone tz;
+//   gettimeofday(&tv, NULL);
+//   int ms = 500;
+//   while (1) {
+//     while ((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ms) {
+//       gettimeofday(&tv, NULL);
+//     }
+//     printf("ms = %d\n", ms);
+//     ms += 500;
+//   }
+//   printf("执行完timer-test\n");
+//   return 0;
+// }
