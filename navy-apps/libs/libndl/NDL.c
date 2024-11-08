5629307419ef1e91c80348995c3aa897986357ec
tracer-ics2024 <tracer@njuics.org>
2024-11-08 21:32:27 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:32:27 up  6:09,  1 user,  load average: 1.03, 1.06, 1.20

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -92,6 +92,7 @@ void NDL_OpenCanvas(int *w, int *h) {
   //保持画布在屏幕中央
   canvas_x=(screen_w - canvas_w) / 2;
   canvas_y=(screen_h - canvas_h) / 2;
+  printf("canvas_x:%d, canvas_y:%d\n",canvas_x,canvas_y);
 }
 
 // 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
