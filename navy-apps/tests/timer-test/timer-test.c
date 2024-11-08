8b39395ad6357dc9785ba02ed9bfca753ba025f4
tracer-ics2024 <tracer@njuics.org>
2024-11-08 16:42:42 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:42:42 up  1:19,  1 user,  load average: 0.85, 1.03, 1.08

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -12,7 +12,7 @@ int main() {
     while ((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ms) {
       gettimeofday(&tv, NULL);
     }
-    printf("time = %f\n", ((float)ms)/1000);
+    printf("ms = %d\n", ms);
     ms += 500;
   }
   printf("执行完timer-test\n");
