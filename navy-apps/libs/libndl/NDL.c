69f356e9231b2be42b77abc6a2d451cc6166e2d9
tracer-ics2024 <tracer@njuics.org>
2024-11-09 10:57:34 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:57:34 up 8 min,  1 user,  load average: 0.98, 1.00, 0.67

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -109,9 +109,9 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   for (int i = 0; i < h && y + i < canvas_h; i++) { //i<h&&y+i<canvas_h是为了保证即使有部分显示不出来也好比越界访问出错好
     int now_line_in_buf = y + canvas_y + i;//确认现在在buf当中第几行
     int now_column_in_buf = x + canvas_x;//确认现在在buf当中的第几列
-    lseek(fd, (now_column_in_buf* screen_w + now_column_in_buf) * sizeof(uint32_t), SEEK_SET);
+    lseek(fd, (now_line_in_buf* screen_w + now_column_in_buf) * sizeof(uint32_t), SEEK_SET);
     write(fd, pixels + i * w,  (w < canvas_w - x ? w : canvas_w - x) *sizeof(uint32_t));//倘若w大于画布宽度减去当前x的话宁愿少贴一点图也不要访问越界
-    // printf("x:%u\ty:%u\tw:%u\th:%u\nfd:%u\nbuf:%u\nlen:%u\n",now_column_in_buf,now_line_in_buf,w,h,fd,pixels + i * w,(w < canvas_w - x ? w : canvas_w - x) *sizeof(uint32_t));
+    // printf("now_column_in_buf:%u\tnow_line_in_buf:%u\tw:%u\th:%u\nfd:%u\nbuf:%u\nlen:%u\n",now_column_in_buf,now_line_in_buf,w,h,fd,pixels + i * w,(w < canvas_w - x ? w : canvas_w - x) *sizeof(uint32_t));
   }
   assert(close(fd) == 0); 
 }
