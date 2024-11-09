10d951159e90baa83cfad27335b29d33f3a18553
tracer-ics2024 <tracer@njuics.org>
2024-11-09 11:28:07 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:28:07 up 39 min,  1 user,  load average: 1.28, 1.39, 1.31

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -45,7 +45,6 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T gpu=io_read(AM_GPU_CONFIG);
    printf("offset:%u\n",offset);
   return snprintf((char*)buf,len,"屏幕宽度:%d 屏幕高度:%d\n",gpu.width, gpu.height);
- 
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
