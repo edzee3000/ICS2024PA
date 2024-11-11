51e88d7635ec8b04dfe18fa16c36af14678f6814
tracer-ics2024 <tracer@njuics.org>
2024-11-11 09:16:55 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:16:55 up 15 min,  1 user,  load average: 1.30, 1.22, 0.96

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -27,9 +27,9 @@ void render() {
   SDL_Surface *slide = SDL_LoadBMP(fname);//这个函数非常耗费时间
   assert(slide);
   static uint32_t count=1;
-  printf("SDL_LoadBMP调用第%u次\n",count);count++;
+  printf("SDL_LoadBMP调用第%u次\n",count);
   SDL_BlitSurface(slide, NULL, screen, NULL); //这里srcrect和dstrect指针都是NULL说明是按照屏幕原封不动移动过来的  这个函数还好但是也挺费时间的
-  printf("SDL_BlitSurface调用第%u次\n",count);
+  printf("SDL_BlitSurface调用第%u次\n",count);count++;
   SDL_UpdateRect(screen, 0, 0, 0, 0);
   SDL_FreeSurface(slide);
 }
