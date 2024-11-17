ab56b665c009a0b847acf1ac0c85912f8c2d7440
tracer-ics2024 <tracer@njuics.org>
2024-11-17 14:20:22 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:20:22 up 42 min,  1 user,  load average: 1.34, 1.20, 1.13

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -4,11 +4,11 @@
 int main() {
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
-
+  printf("目前正常\n");
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-  printf("目前正常\n");
+
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
