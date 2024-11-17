2a7be04846cde0f1c0878912c1d1840f797677f8
tracer-ics2024 <tracer@njuics.org>
2024-11-17 13:24:01 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:24:01 up  1:53,  1 user,  load average: 2.19, 1.69, 1.51

--- navy-apps/libs/libSDL_image/src/stb_image.h
+++ navy-apps/libs/libSDL_image/src/stb_image.h
@@ -930,6 +930,7 @@ STBIDEF stbi_uc *stbi_load_from_memory(stbi_uc const *buffer, int len, int *x, i
 {
    stbi__context s;
    stbi__start_mem(&s,buffer,len);
+   printf("stbi__load_and_postprocess_8bit之前没有问题\n");
    return stbi__load_and_postprocess_8bit(&s,x,y,comp,req_comp);
 }
 
