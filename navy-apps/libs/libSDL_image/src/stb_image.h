a7b19785306e69060656c34b9036b8b614b85e10
tracer-ics2024 <tracer@njuics.org>
2024-11-17 13:25:00 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:25:00 up  1:54,  1 user,  load average: 2.31, 1.80, 1.55

--- navy-apps/libs/libSDL_image/src/stb_image.h
+++ navy-apps/libs/libSDL_image/src/stb_image.h
@@ -867,6 +867,7 @@ static void stbi__vertical_flip_slices(void *image, int w, int h, int z, int byt
 static unsigned char *stbi__load_and_postprocess_8bit(stbi__context *s, int *x, int *y, int *comp, int req_comp)
 {
    stbi__result_info ri;
+   printf("stbi__load_main之前没有问题\n");
    void *result = stbi__load_main(s, x, y, comp, req_comp, &ri, 8);
 
    if (result == NULL)
@@ -874,7 +875,7 @@ static unsigned char *stbi__load_and_postprocess_8bit(stbi__context *s, int *x, 
 
    // it is the responsibility of the loaders to make sure we get either 8 or 16 bit.
    STBI_ASSERT(ri.bits_per_channel == 8 || ri.bits_per_channel == 16);
-   printf("if (ri.bits_per_channel != 8)之前没有问题\n");
+   
    if (ri.bits_per_channel != 8) {
       result = stbi__convert_16_to_8((stbi__uint16 *) result, *x, *y, req_comp == 0 ? *comp : req_comp);
       ri.bits_per_channel = 8;
