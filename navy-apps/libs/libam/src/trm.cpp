928c617155759d2bafa854b2b393b139070590ec
231300027-Zhu Shihang <231300027@smail.nju.edu.cn>
2024-11-17 18:19:15 +0800

    还是不会实现将amkernels编译到navy上  下次得好好花时间看代码重新理解了

--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -1,9 +1,26 @@
 #include <am.h>
 
+
+#include <klib-macros.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// extern char _heap_start;
+// int main(const char *args);
+
 Area heap;
 
+
 void putch(char ch) {
+  // outb(SERIAL_PORT, ch);
+  printf("%c",ch);
 }
 
 void halt(int code) {
+ 
+  exit(code);
+
+  // should not reach here
+  while (1);
 }
+ 
