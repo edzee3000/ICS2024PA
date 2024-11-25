43f717c313d7736c41a77232bb5c29ef1e333b2f
tracer-ics2024 <tracer@njuics.org>
2024-11-25 22:20:51 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:20:51 up  9:31,  1 user,  load average: 1.20, 1.07, 1.02

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -25,33 +25,33 @@
 // static void __am_net_config (AM_NET_CONFIG_T *cfg)    { cfg->present = false; }
 
 
-typedef void (*handler_t)(void *buf);
-//下面lut里面的不是设备寄存器而是抽象寄存器
-static void *lut[128] = { //lut的意思是 "lookup table"（查找表)
-  [AM_TIMER_CONFIG] = __am_timer_config,
-  [AM_TIMER_RTC   ] = __am_timer_rtc,
-  [AM_TIMER_UPTIME] = __am_timer_uptime,
-  [AM_INPUT_CONFIG] = __am_input_config,
-  [AM_INPUT_KEYBRD] = __am_input_keybrd,  //定义了一个AM_INPUT_KEYBRD抽象寄存器
-  [AM_GPU_CONFIG  ] = __am_gpu_config,
-  [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,  //__am_gpu_fbdraw这个函数在nemu里面就是用来实现同步屏幕的功能
-  [AM_GPU_STATUS  ] = __am_gpu_status,
-  [AM_UART_CONFIG ] = __am_uart_config,
-  [AM_AUDIO_CONFIG] = __am_audio_config,
-  [AM_AUDIO_CTRL  ] = __am_audio_ctrl,
-  [AM_AUDIO_STATUS] = __am_audio_status,
-  [AM_AUDIO_PLAY  ] = __am_audio_play,
-  [AM_DISK_CONFIG ] = __am_disk_config,
-  [AM_DISK_STATUS ] = __am_disk_status,
-  [AM_DISK_BLKIO  ] = __am_disk_blkio,
-  [AM_NET_CONFIG  ] = __am_net_config,
-};
+// typedef void (*handler_t)(void *buf);
+// //下面lut里面的不是设备寄存器而是抽象寄存器
+// static void *lut[128] = { //lut的意思是 "lookup table"（查找表)
+//   [AM_TIMER_CONFIG] = __am_timer_config,
+//   [AM_TIMER_RTC   ] = __am_timer_rtc,
+//   [AM_TIMER_UPTIME] = __am_timer_uptime,
+//   [AM_INPUT_CONFIG] = __am_input_config,
+//   [AM_INPUT_KEYBRD] = __am_input_keybrd,  //定义了一个AM_INPUT_KEYBRD抽象寄存器
+//   [AM_GPU_CONFIG  ] = __am_gpu_config,
+//   [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,  //__am_gpu_fbdraw这个函数在nemu里面就是用来实现同步屏幕的功能
+//   [AM_GPU_STATUS  ] = __am_gpu_status,
+//   [AM_UART_CONFIG ] = __am_uart_config,
+//   [AM_AUDIO_CONFIG] = __am_audio_config,
+//   [AM_AUDIO_CTRL  ] = __am_audio_ctrl,
+//   [AM_AUDIO_STATUS] = __am_audio_status,
+//   [AM_AUDIO_PLAY  ] = __am_audio_play,
+//   [AM_DISK_CONFIG ] = __am_disk_config,
+//   [AM_DISK_STATUS ] = __am_disk_status,
+//   [AM_DISK_BLKIO  ] = __am_disk_blkio,
+//   [AM_NET_CONFIG  ] = __am_net_config,
+// };
 
-static void fail(void *buf) { panic("access nonexist register"); }
+// static void fail(void *buf) { panic("access nonexist register"); }
 
 bool ioe_init() {
-  for (int i = 0; i < LENGTH(lut); i++)
-    if (!lut[i]) lut[i] = fail;
+  // for (int i = 0; i < LENGTH(lut); i++)
+  //   if (!lut[i]) lut[i] = fail;
   //这里ioe_init的实现直接照搬abstract-machine/am/src/platform/nemu/ioe/ioe.c当中的实现会不会有问题？？？
   // __am_gpu_init();  
   // __am_timer_init();
@@ -62,9 +62,9 @@ bool ioe_init() {
 // void ioe_read (int reg, void *buf) { int fd = open("/dev/am_ioe", 0, 0); lseek(fd, reg, SEEK_SET);  read(fd, buf, 0);close(fd); }
 // void ioe_write(int reg, void *buf) {  int fd = open("/dev/am_ioe", 0, 0);lseek(fd, reg, SEEK_SET);write(fd, buf, 0);close(fd);}
  
-void ioe_read (int reg, void *buf) { ((handler_t)lut[reg])(buf); }
-void ioe_write(int reg, void *buf) { ((handler_t)lut[reg])(buf); }
-// void ioe_read (int reg, void *buf) {}
-// void ioe_write(int reg, void *buf) {}
+// void ioe_read (int reg, void *buf) { ((handler_t)lut[reg])(buf); }
+// void ioe_write(int reg, void *buf) { ((handler_t)lut[reg])(buf); }
+void ioe_read (int reg, void *buf) {}
+void ioe_write(int reg, void *buf) {}
 
 
