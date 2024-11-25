3b95230688333c0dbb99862b1052c1643777d12e
tracer-ics2024 <tracer@njuics.org>
2024-11-25 22:05:28 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:05:28 up  9:15,  1 user,  load average: 0.89, 0.52, 0.87

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -37,8 +37,9 @@ void init_proc() {
   // naive_uload(NULL,"/bin/menu");
   // naive_uload(NULL,"/bin/nterm");
   // naive_uload(NULL,"/bin/bird");
-  naive_uload(NULL,"/bin/pal");
+  // naive_uload(NULL,"/bin/pal");
   // naive_uload(NULL,"/bin/typing-game");
+   naive_uload(NULL,"/bin/coremark");
 }
 
 Context* schedule(Context *prev) {
