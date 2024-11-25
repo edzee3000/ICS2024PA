0ea624f094ea3d5c08218a8dd14b3764067ecee2
tracer-ics2024 <tracer@njuics.org>
2024-11-25 22:19:53 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:19:53 up  9:30,  1 user,  load average: 1.08, 1.04, 1.01

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -28,23 +28,23 @@
 typedef void (*handler_t)(void *buf);
 //下面lut里面的不是设备寄存器而是抽象寄存器
 static void *lut[128] = { //lut的意思是 "lookup table"（查找表)
-  // [AM_TIMER_CONFIG] = __am_timer_config,
-  // [AM_TIMER_RTC   ] = __am_timer_rtc,
-  // [AM_TIMER_UPTIME] = __am_timer_uptime,
-  // [AM_INPUT_CONFIG] = __am_input_config,
-  // [AM_INPUT_KEYBRD] = __am_input_keybrd,  //定义了一个AM_INPUT_KEYBRD抽象寄存器
-  // [AM_GPU_CONFIG  ] = __am_gpu_config,
-  // [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,  //__am_gpu_fbdraw这个函数在nemu里面就是用来实现同步屏幕的功能
-  // [AM_GPU_STATUS  ] = __am_gpu_status,
-  // [AM_UART_CONFIG ] = __am_uart_config,
-  // [AM_AUDIO_CONFIG] = __am_audio_config,
-  // [AM_AUDIO_CTRL  ] = __am_audio_ctrl,
-  // [AM_AUDIO_STATUS] = __am_audio_status,
-  // [AM_AUDIO_PLAY  ] = __am_audio_play,
-  // [AM_DISK_CONFIG ] = __am_disk_config,
-  // [AM_DISK_STATUS ] = __am_disk_status,
-  // [AM_DISK_BLKIO  ] = __am_disk_blkio,
-  // [AM_NET_CONFIG  ] = __am_net_config,
+  [AM_TIMER_CONFIG] = __am_timer_config,
+  [AM_TIMER_RTC   ] = __am_timer_rtc,
+  [AM_TIMER_UPTIME] = __am_timer_uptime,
+  [AM_INPUT_CONFIG] = __am_input_config,
+  [AM_INPUT_KEYBRD] = __am_input_keybrd,  //定义了一个AM_INPUT_KEYBRD抽象寄存器
+  [AM_GPU_CONFIG  ] = __am_gpu_config,
+  [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,  //__am_gpu_fbdraw这个函数在nemu里面就是用来实现同步屏幕的功能
+  [AM_GPU_STATUS  ] = __am_gpu_status,
+  [AM_UART_CONFIG ] = __am_uart_config,
+  [AM_AUDIO_CONFIG] = __am_audio_config,
+  [AM_AUDIO_CTRL  ] = __am_audio_ctrl,
+  [AM_AUDIO_STATUS] = __am_audio_status,
+  [AM_AUDIO_PLAY  ] = __am_audio_play,
+  [AM_DISK_CONFIG ] = __am_disk_config,
+  [AM_DISK_STATUS ] = __am_disk_status,
+  [AM_DISK_BLKIO  ] = __am_disk_blkio,
+  [AM_NET_CONFIG  ] = __am_net_config,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
