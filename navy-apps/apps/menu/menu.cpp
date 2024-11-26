484d6c09a3aeb0796a3706276ed823ef2bb1efab
tracer-ics2024 <tracer@njuics.org>
2024-11-26 21:45:44 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:45:44 up  5:12,  1 user,  load average: 2.45, 1.47, 1.27

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -83,7 +83,7 @@ int main(int argc, char *argv[], char *envp[]) {
     display_menu(i_max);
 
     
-assert(0);
+
     SDL_Event e;
     
     do {
@@ -150,6 +150,7 @@ static void display_menu(int n) {
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
+  assert(0);
   printf("Available applications:\n");
   char buf[80];
   int i;
