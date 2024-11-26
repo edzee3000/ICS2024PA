3cc9c43b20388f81881267693eb29130df3b8ebe
tracer-ics2024 <tracer@njuics.org>
2024-11-26 21:51:41 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:51:41 up  5:18,  1 user,  load average: 2.30, 1.90, 1.51

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -76,7 +76,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   
   switch (Pixel_Bit)
   {case 32: NDL_DrawRect((uint32_t*)(s->pixels),x,y,w,h);break;
-    case 8:  for (int i = 0; i < h; i ++) {for (int j = 0; j < w; j ++) //找出pixel对应调色盘索引对应的值
+    case 8: assert(0); for (int i = 0; i < h; i ++) {for (int j = 0; j < w; j ++) //找出pixel对应调色盘索引对应的值
     {uint8_t pixel_index= s->pixels[(i+y)*s->w+j+x]; //注意这里的pixel_index是
     // local_pixels[i * w + j]=colors[pixel_index].val;
     //#############################################################################
