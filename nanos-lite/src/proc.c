42f97fa76b3a987147f6f2e11141d80109fe904c
tracer-ics2024 <tracer@njuics.org>
2024-12-02 21:18:02 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:18:02 up  8:42,  1 user,  load average: 0.81, 1.29, 1.42

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -19,9 +19,9 @@ void hello_fun(void *arg) {
   static int j = 1;
   while (1) {
     // if(current==&pcb[0]) printf("目前执行的线程为:pcb[0]\n"); else if(current==&pcb[1]) printf("目前执行的线程为:pcb[1]\n");
-    for(int i=0;i<MAX_NR_PROC;i++){if(current==&pcb[i])printf("目前执行的线程为:pcb[%d]",i); }
+    // for(int i=0;i<MAX_NR_PROC;i++){if(current==&pcb[i])printf("目前执行的线程为:pcb[%d]",i); }
     
-    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg,  j);
+    Log("Hello World from Nanos-lite with arg '%p' for the %uth time!", (uintptr_t)arg,  j);
     j ++;
     printf("j:%d\n",j);
     assert(0);
