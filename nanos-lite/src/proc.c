cae8390dedb79400dd679269f01f226d8e1e08d9
tracer-ics2024 <tracer@njuics.org>
2024-12-04 21:37:56 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:37:56 up  1:12,  1 user,  load average: 1.32, 1.09, 0.75

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -39,7 +39,7 @@ void init_proc() {
   context_kload(&pcb[0], hello_fun, &pcb[0]);
   // context_kload(&pcb[1], hello_fun, &pcb[1]);
   context_uload(&pcb[1], "/bin/hello");
-  context_uload(&pcb[1], "/bin/pal");
+  // context_uload(&pcb[1], "/bin/pal");
   switch_boot_pcb();
   
   Log("Initializing processes...");
