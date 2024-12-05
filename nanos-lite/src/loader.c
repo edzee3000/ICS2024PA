7d4397275ade7d24cf1ebe22bbfc2bb5709f8bce
tracer-ics2024 <tracer@njuics.org>
2024-12-05 13:04:22 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:04:22 up 54 min,  1 user,  load average: 2.52, 1.70, 1.30

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -148,11 +148,11 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   // 对齐到 uintptr_t 边界
   user_stack = (uintptr_t*)((uintptr_t)user_stack & ~(sizeof(uintptr_t) - 1));
   // 将 argv 和 envp 指针拷贝到用户栈
-      assert(0);
   user_stack -= (argc + envc + 4);  // +4 为 NULL 结尾和 argc/envc 的值
   // uintptr_t* user_argv = user_stack;
   // 设置 argc 的值
   user_stack[0] = argc;
+        assert(0);
   // 设置 argv 指针
   for (int i = 0; i < argc; i++) {
     user_stack[i + 1] = (uintptr_t)heap.end - (argc - i - 1) * sizeof(uintptr_t);}
