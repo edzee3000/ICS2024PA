60702db293a4d9331ccfa038a23dbe344362129c
tracer-ics2024 <tracer@njuics.org>
2024-12-05 22:10:17 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:10:17 up 10:00,  1 user,  load average: 1.29, 1.06, 1.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -243,7 +243,7 @@ void draw_ustack(uintptr_t* us_top, uintptr_t* us_end, int argc, int envc ,char 
 {
   
   int num = us_end - us_top;
-  printf("+-----------------------------------------+\n%x: \t\t<---- ustack.end\n",us_end);
+  printf("+-----------------------------------------+\n%x: \t\t\t<---- ustack.end\n",us_end);
   us_end--;
   for(int i=0;i<num;i++)
   {
@@ -252,9 +252,9 @@ void draw_ustack(uintptr_t* us_top, uintptr_t* us_end, int argc, int envc ,char 
     else if(i<num-1 && i>num-argc-2)
     {printf("| %x:|%x| \t<---- argv[%d]\n",(us_end-i),*(us_end-i), num-i-2 );}
     else if(i == num-argc-2)
-    {printf("| %x:|%x| \t<---- NULL\n",(us_end-i),*(us_end-i));}
+    {printf("| %x:|%x| \t\t<---- NULL\n",(us_end-i),*(us_end-i));}
     else if (i == num-1)
-    {printf("| %x:|%x| \t<---- argc & cp->GPRx\n",(us_end-i),*(us_end-i));}
+    {printf("| %x:|%x| \t\t<---- argc & cp->GPRx\n",(us_end-i),*(us_end-i));}
     else
     {
       printf("| %x:|%x|\n",(us_end-i),*(us_end-i));
