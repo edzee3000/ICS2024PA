693067ce3cc3d0f59e59ba288213aea03ef629bd
tracer-ics2024 <tracer@njuics.org>
2024-12-18 10:23:47 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:23:47 up 10 min,  1 user,  load average: 1.21, 1.25, 0.79

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -39,7 +39,7 @@ void hello_fun(void *arg) {
 
 
 // static char *args_pal[] = {"/bin/pal", "--skip", NULL};
-static char *args_menu[] = {"/bin/menu", NULL, NULL};
+// static char *args_menu[] = {"/bin/menu", NULL, NULL};
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, &pcb[0]);
@@ -47,7 +47,8 @@ void init_proc() {
   // context_uload(&pcb[1], "/bin/hello", NULL ,NULL);
   // context_uload(&pcb[1], "/bin/pal");
   // context_uload(&pcb[1], "/bin/pal", args_pal ,NULL);
-  context_uload(&pcb[1], "/bin/menu", args_menu ,NULL);
+  // context_uload(&pcb[1], "/bin/menu", args_menu ,NULL);
+  context_uload(&pcb[1], "/bin/exec-test", NULL ,NULL);
   // context_uload(&pcb[1], "/bin/pal", NULL ,NULL);
   switch_boot_pcb();
   
