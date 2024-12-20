a850c91f3df7d83584f7931dba1a7d46a13d9485
tracer-ics2024 <tracer@njuics.org>
2024-12-20 19:08:54 +0800

    >  compile NEMU 231300027 朱士杭 Linux edzee3000-ASUS-TUF-Gaming-F16-FX607JV-FX607JV 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:08:54 up  6:52,  1 user,  load average: 1.71, 1.49, 1.22

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -27,11 +27,13 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {  //触发了一个异常/中断 
   #ifdef CONFIG_ETRACE
   etrace_errors(NO, epc);
   #endif
+#ifdef TIME_INTR
   // 保存当前的 MIE(mstatus的第3位) 到 MPIE(mstatus的第7位)  注意还有第0位
   word_t MIE=MIE_VALUE;
   cpu.CSRs.mstatus = (cpu.CSRs.mstatus & ~(1 << MPIE_OFFSET)) | (MIE << MPIE_OFFSET) ;
   // 将 MIE 置为 0
   cpu.CSRs.mstatus &= ~(1 << MIE_OFFSET);
+#endif
 
   // printf("NO的值为:%d\n",NO);
   switch (NO)
@@ -39,7 +41,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {  //触发了一个异常/中断 
   case 0:case 1:case 2:case 3:case 4:case 5:case 6:case 7:case 8:case 9:case 10:
   case 11:case 12:case 13:case 14:case 15:case 16:case 17:case 18:case 19:  //先暂时默认所有的都需要+4   要是之后有需要调整的再修改……这个一开始只有case0和1结果又报错了……唉……无语死了
   epc+=4; break;   //当case为0和1的时候同样需要epc+=4否则又重新陷入ecall了……
+#ifdef TIME_INTR
   case IRQ_TIMER: break;//如果对应的是IRQ_TIMER 0x80000007 错误号的话那我就可以不用epc+4了  这样也就减免了cpu-exec当中的epc-4的操作
+#endif
   default: panic("NO错误号无定义\n");  break;}
 
   cpu.CSRs.mcause=NO;//存储触发异常的原因NO
